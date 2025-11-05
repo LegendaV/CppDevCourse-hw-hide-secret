f550bd866be97ef1b4690df1d89a5b27e4661e84
LegendaV <115637903+LegendaV@users.noreply.github.com>
2025-11-06 02:17:48 +0500

    check valid

--- hide-secret.cpp
+++ hide-secret.cpp
@@ -24,8 +24,25 @@ int check_and_change(char* const text, const char* const secret,
     return shift + i;
 }
 
+int get_string_length(const char* const str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
 void hide_secret(char* const text, const char* const secret)
 {
+    const int text_len = get_string_length(text);
+    const int secret_len = get_string_length(secret);
+    if (text_len <= 0 || secret_len <= 0 || text_len < secret_len)
+    {
+        return;
+    }
+
     int i = 0;
     while (text[i] != '\0')
     {
