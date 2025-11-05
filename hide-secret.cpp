71ee7e4cedc1989e9bc529c5abc64740596c58f3
LegendaV <115637903+LegendaV@users.noreply.github.com>
2025-11-06 01:58:01 +0500

    Update hide-secret.cpp

--- hide-secret.cpp
+++ hide-secret.cpp
@@ -1,21 +1,27 @@
-void check_and_change(char* const text, const char* const secret, const int shift)
+int check_and_change(char* const text, const char* const secret,
+    const int shift)
 {
     int i = 0;
     while (secret[i] != '\0')
     {
         if (secret[i] != text[i + shift])
         {
-            return;
+            return shift;
         }
         i++;
     }
+    const int secret_len = i;
+
 
     i = 0;
-    while (secret[i] != '\0')
+    while (i < secret_len && secret[i] != '\0')
     {
         text[i + shift] = 'x';
         i++;
+        i = check_and_change(text, secret, i + shift) - shift;
     }
+
+    return shift + i;
 }
 
 void hide_secret(char* const text, const char* const secret)
@@ -23,6 +29,6 @@ void hide_secret(char* const text, const char* const secret)
     int i = 0;
     while (text[i] != '\0')
     {
-        check_and_change(text, secret, ++i);
+        i = check_and_change(text, secret, ++i);
     }
 }
